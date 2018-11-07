9e2394e672a7917a238480abcd4006d55e2bff0b
Pedro Muraki <pedromuraki@gmail.com>
2018-11-06 23:20:29 -0200

    changed title and set https config

--- src/js/constants/songkick.js
+++ src/js/constants/songkick.js
@@ -1,5 +1,5 @@
 import SongkickJS from 'songkick-js';
 
-const songkick = new SongkickJS('KwvChI9jW9D3aYSd', false);
+const songkick = new SongkickJS('KwvChI9jW9D3aYSd', true);
 
 export default songkick;
